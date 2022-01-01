18fe75667c7cea5c84d613aee966adda24f9e3ba
ljj9245 <ljj9245@yonsei.ac.kr>
2022-01-01 19:10:39 +0900

    problem solving 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+
+import java.io.*;
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        br.close();
+        String[] arr_s = s.split(" ");
+        int count = arr_s.length;
+        if(arr_s[0]==" ") {
+            count = count-1;
+        }
+        System.out.println(arr_s[0]+"nmh");
     }
 }
+
+
