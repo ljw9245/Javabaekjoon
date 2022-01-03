9f409ce3d15a0d050e07932e3a2cf282f971c7b7
ljw9245 <ljj9245@yonsei.ac.kr>
2022-01-04 00:37:51 +0900

    test commit23

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner myscanner = new Scanner(System.in);
-        int num = myscanner.nextInt();
-    }
-    boolean check(String s) {
-        boolean c = false;
-
-
-        return c;
+        long a = myscanner.nextInt();
+        myscanner.close();
+        int n = 1;
+        while (a >(( n *n-n)/2) + 1) {
+            if (a > (( n *n-n)/2) + 1) {
+                n++;
+            }
+        }
+        long q = (( n *n-n)/2) + 1;
+        if (a-q==0) {
+            System.out.println("1/"+n);
+        }
     }
 }
-
 
