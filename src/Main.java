7f45c31d347043b26d17a55cdee8a38c53ad3fb9
ljj9245 <ljj9245@yonsei.ac.kr>
2022-01-13 17:11:38 +0900

    problem solving 3

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,49 @@
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.Objects;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner ms = new Scanner(System.in);
-        int num_case = ms.nextInt();
-        for(int i=0 ; i<num_case;i++) {
-            int h = ms.nextInt();
-            int room = ms.nextInt();
-            int p = ms.nextInt();
-            int a = p%h;
-            int b = p/h;
-            if(h<10) {
-                if (a == 0) {
-                    a = h;
-                }
-                if (b < 10) {
-                    System.out.println(a+"0"+(b+1));
-                } else {
-                    System.out.println(a+""+(b+1));
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        StringTokenizer st = new StringTokenizer(s1);
+        int num_row = Integer.parseInt(st.nextToken());
+        int num_col = Integer.parseInt(st.nextToken());
+        String[][] chess_board = new String[num_row][num_col];
+        for(int i =0 ; i<num_row;i++) {
+            String s2 = br.readLine();
+            String[] arr = s2.split("");
+            for(int j=0 ; j<num_col;j++) {
+                chess_board[i][j] = arr[j];
             }
-            else {
-                if (a==0) {
-                    a = h;
+        }
+        int count =0;
+        int count1=0;
+        int count2=0;
+
+        for(int i=0 ; i<num_row;i++) {
+            for(int j =0 ; j<num_col;j++) {
+                if((i+j)%2==0) {
+                    if(!Objects.equals(chess_board[i][j], "W")) {
+                        count1++;
+                    }
+                    else {
+                        count2++;
+                    }
                 }
-                if (b < 10) {
-                    System.out.println(a+"0"+(b+1));
-                } else {
-                    System.out.println(a+""+(b+1));
+                else {
+                    if(!Objects.equals(chess_board[i][j], "B")) {
+                        count1++;
+                    }
+                    else {
+                        count2++;
+                    }
                 }
             }
         }
+        count = Math.min(count1,count2);
+        System.out.println(count);
     }
 }
 
