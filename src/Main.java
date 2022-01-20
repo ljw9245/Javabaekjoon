d9a57dc2bdf37eb90b807cee3da9b702497916b2
ljw9245 <ljj9245@yonsei.ac.kr>
2022-01-20 12:21:58 +0900

    sorting algorithm.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,34 @@
-
 import java.util.Scanner;
 
-public class Main {
-    public static int[] arr;
-    public static boolean[] visited;
-
-    public static void main(String[] args)  {
+public class Main{
+    public static int n;
+    public static int num_case;
+    public static int count;
+    public static boolean[][] chess_board;
+    public static void main(String[] args) {
         Scanner ms = new Scanner(System.in);
-        int n = ms.nextInt();
-        int m = ms.nextInt();
-        arr = new int[m];
-        visited = new boolean[n];
-        dfs(n,m,0);
+        n = ms.nextInt();
+        chess_board = new boolean[n][n];
+        nqueen();
     }
-    public static void dfs(int n , int m, int depth) {
-        if(depth==m) {
-            for(int val : arr) {
-                System.out.print(val + " ");
+
+    public static void nqueen() {
+        if(count==n) {
+            System.out.println(num_case);
+        }
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                if(!chess_board[i][j]) {
+                    chess_board[i][j] = true;
+                    count++;
+                    for(int k=0; k<n ; k++) {
+                        chess_board[i][k] = true;
+                        chess_board[k][j] = true;
+                    }
+
+                }
             }
-            System.out.println();
-            return;
         }
 
-        for(int i=0; i<n ; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i+1;
-                dfs(n,m,depth+1);
-                visited[i] = false;
-            }
-        }
     }
 }
-
